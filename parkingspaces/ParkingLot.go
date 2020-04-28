8cba98127a17ccde1734c4c27c52c708e894419d
tejaswayadav <tejaswayadav@gmail.com>
2020-04-28 14:25:33 +0530

    Changes to Add and Remove functionalities.

--- parkingspaces/ParkingLot.go
+++ parkingspaces/ParkingLot.go
@@ -2,7 +2,6 @@ package parkingspaces
 
 import (
 	"fmt"
-	"github.com/CarParkingSystem/utils"
 	"github.com/CarParkingSystem/vehicles"
 	"strconv"
 )
@@ -12,7 +11,7 @@ type ParkingLot struct {
 	capacity         int
 	spaceLeft        int
 	parkingAllotment map[string]vehicles.Car
-	parkedVehiclesLicensePlateList utils.StringList
+	parkedVehiclesLicensePlateList []string
 }
 
 func parkingAllotmentCreator(capacity int) map[string]vehicles.Car {
@@ -29,7 +28,11 @@ func (p ParkingLot) GetParkingName() string {
 }
 
 func (p *ParkingLot) GetParkingAllotment() map[string]vehicles.Car{
-	return p.parkingAllotment
+	return (*p).parkingAllotment
+}
+
+func (p *ParkingLot) GetParkedVehicleLicensePlateList() []string{
+	return (*p).parkedVehiclesLicensePlateList
 }
 
 func GetParkingLot(name string, capacity int) ParkingLot {
@@ -58,13 +61,22 @@ func (p *ParkingLot) GetParkingSpotDetails(parkingSpot string) {
 	}
 }
 
+func (p *ParkingLot) vehicleInParkingLot(c vehicles.Car) bool{
+	for _, v := range (*p).parkedVehiclesLicensePlateList{
+		if v == c.GetLicensePlate(){
+			return true
+		}
+	}
+	return false
+}
+
 func (p *ParkingLot) AddCar(c vehicles.Car) {
-	if p.spaceLeft <= 0 {
-		fmt.Println("Parking Space is currently not available in this Parking Lot.")
+	if (*p).spaceLeft <= 0 {
+		fmt.Printf("Car %s %s with plates %s cannot be parked currently. Parking Space not available in this Parking Lot.\n", c.GetCarManufacturer(), c.GetCarName(), c.GetLicensePlate())
 		return
 	} else {
-		for k, v := range p.parkingAllotment {
-			for _, val := range p.parkedVehiclesLicensePlateList {
+		for k, v := range (*p).parkingAllotment {
+			for _, val := range (*p).parkedVehiclesLicensePlateList {
 				if val == c.GetLicensePlate(){
 					fmt.Println("Vehicle already present in the parking lot.")
 					return
@@ -73,11 +85,11 @@ func (p *ParkingLot) AddCar(c vehicles.Car) {
 				}
 			}
 			if v.GetCarName() == "" {
-				p.spaceLeft = p.spaceLeft - 1
-				p.parkingAllotment[k] = c
-				p.parkedVehiclesLicensePlateList = append(p.parkedVehiclesLicensePlateList, c.GetLicensePlate())
+				(*p).spaceLeft = (*p).spaceLeft - 1
+				(*p).parkingAllotment[k] = c
+				(*p).parkedVehiclesLicensePlateList = append((*p).parkedVehiclesLicensePlateList, c.GetLicensePlate())
 				fmt.Printf("Car %s %s with plates %s is parked at %s in %s\n", c.GetCarManufacturer(), c.GetCarName(), c.GetLicensePlate(), k, p.name)
-				fmt.Println("Space left is: ", p.spaceLeft)
+				fmt.Println("Space left is: ", (*p).spaceLeft)
 				return
 			}
 		}
@@ -85,26 +97,29 @@ func (p *ParkingLot) AddCar(c vehicles.Car) {
 }
 
 func (p *ParkingLot) RemoveCar(c vehicles.Car) {
-	if len(p.parkedVehiclesLicensePlateList) == 0 {
+	if len((*p).parkedVehiclesLicensePlateList) == 0 {
 		fmt.Println("This Parking Lot is empty.")
 		return
 	} else {
-		if p.parkedVehiclesLicensePlateList.Contains(c.GetLicensePlate()) {
-			for k, _ := range p.parkingAllotment {
-				p.parkingAllotment[k] = vehicles.Car{}
-				var indexToBeDeleted int
-				for i, v := range p.parkedVehiclesLicensePlateList{
-					if v == c.GetLicensePlate() {
-						indexToBeDeleted = i
+		if  (*p).vehicleInParkingLot(c){
+			for k, v := range (*p).parkingAllotment {
+				if v == c {
+					(*p).parkingAllotment[k] = vehicles.Car{}
+					var indexToBeDeleted int
+					for i, v := range (*p).parkedVehiclesLicensePlateList{
+						if v == c.GetLicensePlate() {
+							indexToBeDeleted = i
+						}
 					}
+					(*p).parkedVehiclesLicensePlateList = append((*p).parkedVehiclesLicensePlateList[:indexToBeDeleted], (*p).parkedVehiclesLicensePlateList[indexToBeDeleted+1:]...)
+					(*p).spaceLeft = (*p).spaceLeft + 1
+					fmt.Printf("Car %s %s with plates %s has left %s\n", c.GetCarManufacturer(), c.GetCarName(), c.GetLicensePlate(), p.name)
+					return
 				}
-				p.parkedVehiclesLicensePlateList = append(p.parkedVehiclesLicensePlateList[:indexToBeDeleted], p.parkedVehiclesLicensePlateList[indexToBeDeleted+1:]...)
-				p.spaceLeft = p.spaceLeft + 1
-				fmt.Printf("Car %s %s with plates %s has left %s\n", c.GetCarManufacturer(), c.GetCarName(), c.GetLicensePlate(), p.name)
-				return
 			}
 		} else {
 			fmt.Printf("Car %s %s with plates %s is not parked at %s\n", c.GetCarManufacturer(), c.GetCarName(), c.GetLicensePlate(), p.name)
+			return
 		}
 	}
 }
