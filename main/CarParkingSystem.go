dad5ca5425ea14ae3645cde9f95fab6989f2f729
tejaswayadav <tejaswayadav@gmail.com>
2020-04-28 14:28:04 +0530

    Changes to Check Availability function.

--- main/CarParkingSystem.go
+++ main/CarParkingSystem.go
@@ -7,13 +7,14 @@ import (
 )
 
 func CheckAvailability(p parkingspaces.ParkingLot) {
-	var emptySpotList []string
+	var EmptySpotList []string
 	for k, v := range p.GetParkingAllotment(){
 		if v.GetCarName() == "" {
-			emptySpotList = append(emptySpotList, k)
+			EmptySpotList = append(EmptySpotList, k)
 		}
 	}
-	fmt.Println("Parking Space Available at: ", emptySpotList)
+	fmt.Println("Parking Space Available at: ", EmptySpotList)
+	return
 }
 
 func main() {
@@ -24,14 +25,16 @@ func main() {
 	c2 := vehicles.GetCar("Figo", "Ford", "MH04BS85")
 	c3 := vehicles.GetCar("DZire", "Maruti Suzuki", "MH04BD12")
 	c4 := vehicles.GetCar("SCross", "Maruti Suzuki", "MH04GH56")
-	c5 := vehicles.GetCar("SX4", "Maruti Suzuki", "MH04BD45")
+	// c5 := vehicles.GetCar("SX4", "Maruti Suzuki", "MH04BD45")
 	p.AddCar(c1)
 	p.AddCar(c2)
 	p.AddCar(c3)
-	p.AddCar(c4)
+	//p.AddCar(c4)
 	p.RemoveCar(c1)
 	p.RemoveCar(c2)
-	p.RemoveCar(c5)
+	//p.RemoveCar(c3)
+	//p.AddCar(c4)
+	p.RemoveCar(c4)
 	CheckAvailability(p)
 }
 
